21ce228f378586b2f9f0e432d29e4564d5df28a3
Arc <33088785+arcbtc@users.noreply.github.com>
2021-12-21 17:58:22 +0000

    updated to Stepans latest fix

--- LNURLVend.ino
+++ LNURLVend.ino
@@ -22,10 +22,9 @@
 ////////////////////////////////////////////////////////
 
 //Server details
-String server = "https://legend.lnbits.com";
-String posId = "UhL9PnuuQ6K3uih6G5DU3a";
-String key = "5HFLiZm8DQ356NHaSJAMJs";
-String currency = "GBP";
+String baseURL = "https://legend.lnbits.com/lnurlpos/api/v1/lnurl/Menv6Rx6Cpnd5nXTPdGfTu";
+String key = "kK4QzkxwN2V8iuyocg6VFj";
+String currency = "USD";
 
 //Products
 String prodOne = "ESP32";
@@ -349,45 +348,79 @@ void to_upper(char * arr){
 }
 
 //////////LNURL AND CRYPTO///////////////
-////VERY KINDLY DONATED BY SNIGIREV!/////
 
-void makeLNURL(){
-  randomPin = random(1000,9999);
+void makeLNURL()
+{
+  randomPin = random(1000, 9999);
   byte nonce[8];
-  for(int i = 0; i < 8;i++){
-    nonce[i] = random(9);
+  for (int i = 0; i < 8; i++)
+  {
+    nonce[i] = random(256);
   }
-  byte payload[8];
-  encode_data(payload, nonce, randomPin, amount);
-  preparedURL = server + "/lnurlpos/api/v1/lnurl/";
-  preparedURL += toHex(nonce,8);
-  preparedURL += "/";
-  preparedURL += toHex(payload, 8);
-  preparedURL += "/";
-  preparedURL += posId;
+  byte payload[51]; // 51 bytes is max one can get with xor-encryption
+  size_t payload_len = xor_encrypt(payload, sizeof(payload), (uint8_t *)key.c_str(), key.length(), nonce, sizeof(nonce), randomPin, amount);
+  preparedURL = baseURL + "?p=";
+  preparedURL += toBase64(payload, payload_len, BASE64_URLSAFE | BASE64_NOPADDING);
   Serial.println(preparedURL);
   char Buf[200];
   preparedURL.toCharArray(Buf, 200);
   char *url = Buf;
-  byte * data = (byte *)calloc(strlen(url)*2, sizeof(byte));
+  byte *data = (byte *)calloc(strlen(url) * 2, sizeof(byte));
   size_t len = 0;
   int res = convert_bits(data, &len, 5, (byte *)url, strlen(url), 8, 1);
-  char * charLnurl = (char *)calloc(strlen(url)*2, sizeof(byte));
+  char *charLnurl = (char *)calloc(strlen(url) * 2, sizeof(byte));
   bech32_encode(charLnurl, "lnurl", data, len);
   to_upper(charLnurl);
   lnurl = charLnurl;
   Serial.println(lnurl);
 }
 
-void encode_data(byte output[8], byte nonce[8], int pin, int amount_in_cents){
-  SHA256 h;
-  h.write(nonce, 8);
-  h.write((byte *)key.c_str(), key.length());
-  h.end(output);
-  output[0] = output[0] ^ ((byte)(pin & 0xFF));
-  output[1] = output[1] ^ ((byte)(pin >> 8));
-  for(int i=0; i<4; i++){
-    output[2+i] = output[2+i] ^ ((byte)(amount_in_cents & 0xFF));
-    amount_in_cents = amount_in_cents >> 8;
+/*
+ * Fills output with nonce, xored payload, and HMAC.
+ * XOR is secure for data smaller than the key size (it's basically one-time-pad). For larger data better to use AES.
+ * Maximum length of the output in XOR mode is 1+1+nonce_len+1+32+8 = nonce_len+43 = 51 for 8-byte nonce.
+ * Payload contains pin, currency byte and amount. Pin and amount are encoded as compact int (varint).
+ * Returns number of bytes written to the output, 0 if error occured.
+ */
+int xor_encrypt(uint8_t * output, size_t outlen, uint8_t * key, size_t keylen, uint8_t * nonce, size_t nonce_len, uint64_t pin, uint64_t amount_in_cents){
+  // check we have space for all the data:
+  // <variant_byte><len|nonce><len|payload:{pin}{amount}><hmac>
+  if(outlen < 2 + nonce_len + 1 + lenVarInt(pin) + 1 + lenVarInt(amount_in_cents) + 8){
+    return 0;
   }
+  int cur = 0;
+  output[cur] = 1; // variant: XOR encryption
+  cur++;
+  // nonce_len | nonce
+  output[cur] = nonce_len;
+  cur++;
+  memcpy(output+cur, nonce, nonce_len);
+  cur += nonce_len;
+  // payload, unxored first - <pin><currency byte><amount>
+  int payload_len = lenVarInt(pin) + 1 + lenVarInt(amount_in_cents);
+  output[cur] = (uint8_t)payload_len;
+  cur++;
+  uint8_t * payload = output+cur; // pointer to the start of the payload
+  cur += writeVarInt(pin, output+cur, outlen-cur); // pin code
+  cur += writeVarInt(amount_in_cents, output+cur, outlen-cur); // amount
+  cur++;
+  // xor it with round key
+  uint8_t hmacresult[32];
+  SHA256 h;
+  h.beginHMAC(key, keylen);
+  h.write((uint8_t *)"Round secret:", 13);
+  h.write(nonce, nonce_len);
+  h.endHMAC(hmacresult);
+  for(int i=0; i < payload_len; i++){
+    payload[i] = payload[i] ^ hmacresult[i];
+  }
+  // add hmac to authenticate
+  h.beginHMAC(key, keylen);
+  h.write((uint8_t *)"Data:", 5);
+  h.write(output, cur);
+  h.endHMAC(hmacresult);
+  memcpy(output+cur, hmacresult, 8);
+  cur += 8;
+  // return number of bytes written to the output
+  return cur;
 }
